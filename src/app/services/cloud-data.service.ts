c9d699dba056468993003dffd47d6d74d8d1554e
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2020-05-02 12:40:17 +0800

    working on update function

--- src/app/services/cloud-data.service.ts
+++ src/app/services/cloud-data.service.ts
@@ -12,12 +12,15 @@ export class CloudDataService {
     });
   }
   getDatas = () => { return this.af.collection("tutorials").snapshotChanges(); }
-  updateData(data) {
-    return this.af
-               .collection("tutorials")
-               .doc(data.payload.doc.id)
-               .update(data.payload.doc.data());
-   }
   deleteData = async (data) => { return this.af.collection("tutorials").doc(data.payload.doc.id).delete(); }
   getTagsData = () => { return this.af.collection("tutorial_tags").valueChanges(); }
+  //experimental
+  updateData(data) {
+    return this.af.collection("tutorials").doc(data.payload.doc.id).set(data.payload.doc.data());
+   }
+  pushIncrementTag = async (tag) =>{
+    return new Promise<any>((res, rej) => {
+      this.af.collection('tutorial_tags', ref => ref.where('tag','==',tag));
+    });
+  }
 }
--- src/app/services/tutorial.service.ts
+++ src/app/services/tutorial.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CloudDataService } from './cloud-data.service';
+import { FormControl, Validators } from '@angular/forms';
 //handle form data
 //call cloud data service
 @Injectable({
@@ -7,7 +8,12 @@ import { CloudDataService } from './cloud-data.service';
 })
 export class TutorialService {
   public datas;
+  public updating:boolean = false;
+  public dataToUpdate;
+  public titFC = new FormControl("", Validators.compose([Validators.required,Validators.pattern("^[A-Z]{1}.*")]));
   constructor(public cd:CloudDataService) { }
   getdatas = () => this.cd.getDatas().subscribe(res => { this.datas = res });
   deleteData = (data) => this.cd.deleteData(data);
+  updatingData = ( data ) => { this.updating = !this.updating; this.dataToUpdate = data }
+  updateData = () => this.cd.updateData(this.dataToUpdate);
 }
