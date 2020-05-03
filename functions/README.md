bf940108d961a7d260c8a1af47e92f67e2346449
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2020-05-03 21:39:14 +0800

    nearly succes cloud function

--- functions/README.md
+++ functions/README.md
@@ -1,4 +1,4 @@
-set GOOGLE_APPLICATION_CREDENTIALS=./code-ajis-admin.json
-export GOOGLE_APPLICATION_CREDENTIALS=./code-ajis-admin.json
+set GOOGLE_APPLICATION_CREDENTIALS=admin-key.json
+export GOOGLE_APPLICATION_CREDENTIALS=admin-key.json
 firebase emulators:start
 firebase emulators:start --only functions
--- functions/index.js
+++ functions/index.js
@@ -1,26 +1,36 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-admin.initializeApp();
 
+admin.initializeApp({
+  credential: admin.credential.applicationDefault()
+});
 const db = admin.firestore();
 
 exports.updateTag = functions.firestore
-    .document('tutorials/{doc-id}')
+    .document('tutorials/{docId}') //cannot have -
     .onCreate( (snap, context) => {
         const newValue = snap.data();
         const tag = newValue.tag;
         db.collection('tutorial_tags').where('tag','==',tag).get().then( (snapshot) => {
             snapshot.forEach( (doc) => { 
               let id = doc.id; 
-              let cumulative = doc.data().cumulative + 1 
-        
-              console.log('id is: ',id, 'cumulative is: '+cumulative);
-              if(id!=null)
-              db.collection('tutorial_tags').doc(id).update({'cumulative': cumulative})
-        
-              db.collection('tutorial_tags').where('tag','==',tag).get().then( (snapshot) => {
-                snapshot.forEach( (doc) => { console.log(doc.id , '=>' , doc.data() )} );
-              });
+              let cumulative = doc.data().cumulative + 1 ;
+              if(id!=null){
+                db.collection('tutorial_tags').doc(id).update({'cumulative': cumulative});
+              }
             } );
         })
     });
+
+// exports.createUser = functions.firestore
+//     .document('users/{userId}')
+//     .onCreate((snap, context) => {
+//       // Get an object representing the document
+//       // e.g. {'name': 'Marie', 'age': 66}
+//       const newValue = snap.data();
+
+//       // access a particular field as you would any JS property
+//       const name = newValue.name;
+
+//       // perform desired operations ...
+//     });
