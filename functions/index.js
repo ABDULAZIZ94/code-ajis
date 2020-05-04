c98a4a601ae0209fb07bacc5fbe8d1da9adbcef3
ABDULAZIZ94 <abdulazizabdulnoor@gmail.com>
2020-05-04 11:19:56 +0800

    success cloud function

--- functions/index.js
+++ functions/index.js
@@ -22,15 +22,3 @@ exports.updateTag = functions.firestore
         })
     });
 
-// exports.createUser = functions.firestore
-//     .document('users/{userId}')
-//     .onCreate((snap, context) => {
-//       // Get an object representing the document
-//       // e.g. {'name': 'Marie', 'age': 66}
-//       const newValue = snap.data();
-
-//       // access a particular field as you would any JS property
-//       const name = newValue.name;
-
-//       // perform desired operations ...
-//     });
